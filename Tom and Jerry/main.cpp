51d13f59681808e3f1b2d56911c747a87a5c69ca
Iliyan0411 <ilian_dd00@abv.bg>
2020-12-02 16:55:32 +0200

    change

--- Tom and Jerry/main.cpp
+++ Tom and Jerry/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
    PTree t;
-   std::queue<char> q, q1, q2;
+   std::queue<char> q, q1, q2, q3;
    q.push('N');
    q.push('N');
    q.push('N');
@@ -26,9 +26,15 @@ int main()
    q2.push('E');
    q2.push('N');
 
+   q3.push('N');
+   q3.push('N');
+   q3.push('E');
+   q3.push('W');
+
    t.add(q);
    t.add(q1);
    t.add(q2);
+   t.add(q3);
 
     std::ofstream out("viz.dot");
     t.viz(out);
