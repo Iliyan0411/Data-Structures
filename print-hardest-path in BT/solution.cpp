345355d497d9e6c3ed517193ca133583e820c151
Iliyan0411 <ilian_dd00@abv.bg>
2020-12-28 18:29:17 +0200

    shortened

--- print-hardest-path in BT/solution.cpp
+++ print-hardest-path in BT/solution.cpp
@@ -12,67 +12,28 @@ struct Node
 };
 
 
-
-void dfs(Node* curr, int sum, int& max, vector<int> currPath, vector<vector<int>>& allPaths)
+void dfs(Node* curr, int sum, int& max, vector<int> currPath, vector<int>& result)
 {
-    if(!curr)
-    {
-        return;
-    }
+    if(!curr) return;
     
     sum += curr->data;
     currPath.push_back(curr->data);
     
-    if(!curr->left && !curr->right && sum > max)
-    {
-        allPaths.push_back(currPath);
-    }
-    
+    if(!curr->left && !curr->right && sum > max) result = currPath;
     if(sum > max) max = sum;
     
-    
-    dfs(curr->left, sum, max, currPath, allPaths);
-    dfs(curr->right, sum, max, currPath, allPaths);
+    dfs(curr->left, sum, max, currPath, result);
+    dfs(curr->right, sum, max, currPath, result);
 }
 
-vector<vector<int>> hardestPath(Node* root)
+vector<int> hardestPath(Node* root)
 {
     int max = root->data;
-    vector<int> currPath;
-    vector<vector<int>> allPaths;
+    vector<int> currPath, result;
     
-    dfs(root, 0, max, currPath, allPaths);
+    dfs(root, 0, max, currPath, result);
     
-    return allPaths;
-}
-
-void printHardestPath(vector<vector<int>> allPaths)
-{
-    int max = 0, maxIndex = 0;
-    
-    for(int i = 0; i < allPaths.size(); ++i)
-    {
-        int sum = 0;
-        for(int j = 0; j < allPaths[i].size(); ++j)
-        {
-            sum += allPaths[i][j];
-        }
-        if(i == 0){
-            maxIndex = i;
-            max = sum;
-        }
-        else{
-            if(sum > max){
-                max = sum;
-                maxIndex = i;
-            }
-        }
-    }
-    
-    for(int elem : allPaths[maxIndex])
-    {
-        cout << elem << " ";
-    }
+    return result;
 }
 
 
@@ -81,8 +42,11 @@ int main()
     Node *root = new Node(5, new Node(4, new Node(2, nullptr, nullptr), nullptr), new Node(7, new Node(8,nullptr,nullptr), 
                 new Node(6, new Node(4,nullptr,nullptr), nullptr)));
 
-    printHardestPath(hardestPath(root));
 
+    for(int elem : hardestPath(root))
+    {
+        std::cout << elem << " ";
+    }
 
 
     return 0;
